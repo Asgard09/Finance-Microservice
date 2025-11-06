4654bdf01a88d7e2fe60dcc33615d1fbfee9528a
Asgard09 <thanhphuc04nh@gmail.com>
2025-11-06 16:00:32 +0700

    Feat: Service 01

--- service01/src/main/java/com/asgard09/service01/Service01Application.java
+++ service01/src/main/java/com/asgard09/service01/Service01Application.java
@@ -2,8 +2,10 @@ package com.asgard09.service01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Service01Application {
 
 	public static void main(String[] args) {
